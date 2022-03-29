a91f4b2aa3bbe358944b0f8a0b3974c01141f895
fjchen7 <fjchen7@gmail.com>
2022-03-29 19:15:55 +0800

    Remove TransactionBuilder

--- core/src/main/java/org/ckbj/TransactionBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.ckbj;
-
-public class TransactionBuilder {
-    protected DataFetcher dataFetcher;
-
-    public TransactionBuilder(DataFetcher dataFetcher) {
-        this.dataFetcher = dataFetcher;
-    }
-}
--- core/src/main/java/org/ckbj/TransactionSender.java
+++
@@ -1,9 +0,0 @@
-package org.ckbj;
-
-import org.ckbj.protocol.type.Transaction;
-
-import java.io.IOException;
-
-public interface TransactionSender {
-    String sendTransaction(Transaction transaction) throws IOException;
-}
