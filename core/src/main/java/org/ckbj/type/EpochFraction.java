c8907a7b81c5bffd9dcb910fe59e4156eceeaabb
fjchen7 <fjchen7@gmail.com>
2022-03-30 20:14:39 +0800

    Extract encode and decode method of EpochFraction

--- core/src/main/java/org/ckbj/type/EpochFraction.java
+++ core/src/main/java/org/ckbj/type/EpochFraction.java
@@ -39,49 +39,57 @@ public class EpochFraction {
         return this;
     }
 
+    public static byte[] encode(EpochFraction src) {
+        // encode: length || index || number
+        //           0  1 || 2  3  || 4  5  6
+        // See: https://github.com/nervosnetwork/rfcs/blob/master/rfcs/0019-data-structures/0019-data-structures.md#header
+        byte[] bytes = new byte[7];
+        encode(bytes, src.number, 4, 6);
+        encode(bytes, src.blockIndex, 2, 3);
+        encode(bytes, src.length, 0, 1);
+        return bytes;
+    }
+
+    public static EpochFraction decode(byte[] src) {
+        if (src.length < 7) {
+            byte[] dest = new byte[7];
+            System.arraycopy(src, 0, dest, 7 - src.length, src.length);
+            src = dest;
+        }
+        EpochFraction epoch = new EpochFraction();
+        epoch.number = decode(src, 4, 6);
+        epoch.blockIndex = decode(src, 2, 3);
+        epoch.length = decode(src, 0, 1);
+        return epoch;
+    }
+
+    private static void encode(byte[] bytes, int number, int start, int end) {
+        int bit = 0;
+        for (int i = end; i >= start; i--) {
+            bytes[i] = (byte) (number >> bit & 0xff);
+            bit += 8;
+        }
+    }
+
+    private static int decode(byte[] bytes, int start, int end) {
+        int number = 0;
+        for (int i = start; i <= end; i++) {
+            number = number * 256 + (bytes[i] & 0xff);
+        }
+        return number;
+    }
+
     protected class TypeAdapter implements JsonSerializer<EpochFraction>, JsonDeserializer<EpochFraction> {
         @Override
         public JsonElement serialize(EpochFraction src, Type typeOfSrc, JsonSerializationContext context) {
-            JsonObject obj = new JsonObject();
-            // encode: length || index || number
-            //           0  1 || 2  3  || 4  5  6
-            // See: https://github.com/nervosnetwork/rfcs/blob/master/rfcs/0019-data-structures/0019-data-structures.md#header
-            byte[] bytes = new byte[7];
-            encode(bytes, src.number, 4, 6);
-            encode(bytes, src.blockIndex, 2, 3);
-            encode(bytes, src.length, 0, 1);
+            byte[] bytes = EpochFraction.encode(src);
             return new JsonPrimitive(Hex.encode(bytes));
         }
 
         @Override
         public EpochFraction deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
             byte[] bytes = Hex.decode(json.getAsString());
-            if (bytes.length < 7) {
-                byte[] copied = bytes;
-                bytes = new byte[7];
-                System.arraycopy(copied, 0, bytes, 7 - copied.length, copied.length);
-            }
-            EpochFraction epoch = new EpochFraction();
-            epoch.number = decode(bytes, 4, 6);
-            epoch.blockIndex = decode(bytes, 2, 3);
-            epoch.length = decode(bytes, 0, 1);
-            return epoch;
-        }
-
-        private int decode(byte[] bytes, int start, int end) {
-            int number = 0;
-            for (int i = start; i <= end; i++) {
-                number = number * 256 + (bytes[i] & 0xff);
-            }
-            return number;
-        }
-
-        private void encode(byte[] bytes, int number, int start, int end) {
-            int bit = 0;
-            for (int i = end; i >= start; i--) {
-                bytes[i] = (byte) (number >> bit & 0xff);
-                bit += 8;
-            }
+            return EpochFraction.decode(bytes);
         }
     }
 }
